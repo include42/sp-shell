812efab60c94967769de955e106a6187aa949d73
gunhee lee <dlrjsgml42@naver.com>
2021-05-31 00:28:34 +0900

    feat: Background 로직 구현
    - 백그라운드 구현을 위해 &의 유무를 확인
    - BG 구조체 구현. 번호, 명령어, 상태, 프로세스 아이디 목록과 그 사이즈를 보유
    - 파이프를 쓰는 경우에는 모든 프로세스 아이디를 가지고 있도록 함. 종료 시 한번에 종료하고, 정지와 재생도 한번에 해야됨.
    - 파이프를 쓰지 않는 경우 아이디 1개만 가지고 있을 예정

--- myshell.c
+++ myshell.c
@@ -2,21 +2,41 @@
 #include "csapp.h"
 #include<errno.h>
 #define MAXARGS   128
+
+#define MAXBG 128
+#define MAXBG_NAME 30
+
 #define MAXPIPES 20
 #define PIPEIN 1
 #define PIPEOUT 2
 
+enum STATE {
+    RUNNING,
+    STOPPED,
+    TERMINATED
+};
+
+typedef struct _BG {
+    int num;
+    char name[MAXBG_NAME];
+    enum STATE state;
+    pid_t pid[MAXPIPES];
+    int pidSize;
+} BG;
+
 /* Function prototypes */
-void createProcess(char *cmdline);
+pid_t createProcess(char *cmdline);
 int myshell_parseline(char *buf, char **argv);
 int builtin_command(char **argv); 
 
-void myshell_piping(char *cmdline);
-void createPipeProcess(char *cmdline, int* fd, int pipetype);
+void myshell_piping(char *cmdline, int *bgSize, BG *bgList);
+pid_t createPipeProcess(char *cmdline, int* fd, int pipetype);
 
 /* $begin shellmain */
 int main() 
 {
+    int bgSize = 0;
+    BG bgList[MAXBG]={};
     char cmdline[MAXLINE]; /* Command line */
 
     while (1) {
@@ -26,7 +46,7 @@ int main()
         if (feof(stdin)){
             exit(0);
         }
-    	myshell_piping(cmdline);
+    	myshell_piping(cmdline, &bgSize, bgList);
     } 
 }
 /* $end shellmain */
@@ -42,8 +62,18 @@ int findPipeCount(char* cmdline) {
     return cnt;
 }
 
+int isBackground(char *cmdline) {
+    int size = strlen(cmdline);
+
+    if(size > 2 && cmdline[size-2] == '&') {
+        cmdline[size-2] = NULL;
+        return 1;
+    }
+    return 0;
+}
+
 /* myshell_piping은 명령어의 pipe 유무를 확인하고, 그 경우에 따라 올바른 연산을 수행하도록 한다. */
-void myshell_piping(char *cmdline) 
+void myshell_piping(char *cmdline, int* bgSize, BG *bgList) 
 {
     int fd[MAXPIPES][2];
     char buf[MAXLINE];   /* Holds modified command line */    
@@ -51,11 +81,43 @@ void myshell_piping(char *cmdline)
     char *temp;
     int pipeCount,i;
 
+    int isBg=0;
+    pid_t pids[MAXPIPES]={0,};
+    int pidSize = 0;
+
     strcpy(buf, cmdline);
     pipeCount = findPipeCount(buf);
+    isBg = isBackground(buf);
+
+    if(isBg) {
+        //백그라운드 실행일 경우, bgList에 정보를 넣는다.
+        //이후, Wait하지 않도록 임의로 조정한다.
+        bgList[(*bgSize)].num = (*bgSize);
+        strncpy(bgList[(*bgSize)].name, buf, MAXBG_NAME);
+        if(strlen(buf) >= MAXBG_NAME) {
+            bgList[(*bgSize)].name[MAXBG_NAME - 1] = NULL;
+        }
+        bgList[(*bgSize)].state = RUNNING;
+    }
     
     if(pipeCount == 0) {
-        createProcess(cmdline);
+        pid_t pid = createProcess(cmdline);
+        int status;
+        if(!isBg){
+            Wait(&status);
+            if(WIFSIGNALED(status)) {
+                /* 
+                자식 프로세스에서 시그널이 발생한 경우, 오류를 처리해 준다. 
+                phase1에서 시그널이 생기는 경우는 exit 처리 경우뿐이므로
+                exit(0)를 해주도록 단순 처리하였다.
+                */
+                exit(0);
+            }
+        } else {
+            bgList[(*bgSize)].pid[0] = pid;
+            bgList[(*bgSize)++].pidSize = 1;
+            printf("[%d] %d\n", (*bgSize), pid);
+        }
         return;
     } else if(pipeCount >= MAXPIPES) { // 파이프가 너무 많을 경우, 해당 명령어를 실행하지 않고 종료한다.
         unix_error("Too Many Pipes...");
@@ -69,16 +131,16 @@ void myshell_piping(char *cmdline)
         }
     }
     seperatedCmdLine = strtok(buf, "|");
-    createPipeProcess(seperatedCmdLine, fd[0], PIPEOUT);
+    pids[pidSize++] = createPipeProcess(seperatedCmdLine, fd[0], PIPEOUT);
     Close(fd[0][1]);
     for(i=1;i<pipeCount;i++) { //pipe의 개수 +1개만큼 조각이 나뉨. 그러나 마지막은 건너뛰므로 pipeCount까지
         seperatedCmdLine = strtok(NULL, "|");
         int tempFd[2] = {fd[i-1][0], fd[i][1]};
-        createPipeProcess(seperatedCmdLine, tempFd, PIPEIN | PIPEOUT);
+        pids[pidSize++] = createPipeProcess(seperatedCmdLine, tempFd, PIPEIN | PIPEOUT);
         Close(fd[i][1]);
     }
     seperatedCmdLine = strtok(NULL, "|");
-    createPipeProcess(seperatedCmdLine, fd[i-1], PIPEIN);
+    pid_t lastPid = createPipeProcess(seperatedCmdLine, fd[i-1], PIPEIN);
 
     /*
     자식 프로세스들을 만들어 준 뒤, 부모 프로세스는 사용하지 않는 fd를 닫아준다.
@@ -89,29 +151,40 @@ void myshell_piping(char *cmdline)
     for(i=0;i<pipeCount;i++){
         Close(fd[i][0]);
     }
+
     int status;
     int exitFlag = 0;
-    for(i=0;i<=pipeCount;i++){
-        Wait(&status);
+    if(!isBg) {
+        for(i=0;i<=pidSize;i++){
+            Wait(&status);
+        }
         if(WIFSIGNALED(status)) {
             exitFlag = 1;
         }
-    }
-    if(exitFlag) {
-        /* 
-        자식 프로세스에서 시그널이 발생한 경우, 오류를 처리해 준다. 
-        phase1에서 시그널이 생기는 경우는 exit 처리 경우뿐이므로
-        exit(0)를 해주도록 단순 처리하였다.
+        if(exitFlag) {
+            /* 
+            자식 프로세스에서 시그널이 발생한 경우, 오류를 처리해 준다. 
+            phase1에서 시그널이 생기는 경우는 exit 처리 경우뿐이므로
+            exit(0)를 해주도록 단순 처리하였다.
 
-        또한, 파이프 연산의 경우 마지막 연산이 exit인 경우에만 exit가 유효하다.
-        이외의 연산은 piping 과정의 일부이므로, 부모 프로세스에 영향을 주지 않는다.
-        그렇기 때문에, exit에 대한 대응은 endProcess에 한하도록 하였다.
-        */
-        exit(0);
+            또한, 파이프 연산의 경우 마지막 연산이 exit인 경우에만 exit가 유효하다.
+            이외의 연산은 piping 과정의 일부이므로, 부모 프로세스에 영향을 주지 않는다.
+            그렇기 때문에, exit에 대한 대응은 endProcess에 한하도록 하였다.
+            */
+            exit(0);
+        }
+    } else {
+        //여기는 bg일때, 마지막 연산(파이프의 끝)에 대해 bg 처리하는 부분임.
+        bgList[(*bgSize)].pidSize = pidSize + 1;
+        for(i=0;i<pidSize;i++) {
+            bgList[(*bgSize)].pid[i] = pids[i];
+        }
+        bgList[(*bgSize)++].pid[i] = lastPid;
+        printf("[%d] %d\n", (*bgSize), lastPid);
     }
 }
 
-void createPipeProcess(char *cmdline, int* fd, int pipetype)
+pid_t createPipeProcess(char *cmdline, int* fd, int pipetype)
 {
     char *argv[MAXARGS]; /* Argument list execve() */
     char buf[MAXLINE];   /* Holds modified command line */
@@ -124,10 +197,10 @@ void createPipeProcess(char *cmdline, int* fd, int pipetype)
     bg = myshell_parseline(buf, argv); 
     /* Ignore empty lines */
     if (argv[0] == NULL) {
-	    return;   
+	    return -1;   
     }
     if (!builtin_command(argv)) {
-        if(Fork() == 0){
+        if((pid = Fork()) == 0){
             if(pipetype & PIPEIN) {
                 Dup2(fd[0], STDIN_FILENO); 
                 Close(fd[0]);
@@ -138,7 +211,6 @@ void createPipeProcess(char *cmdline, int* fd, int pipetype)
             }      
             if (!strcmp(argv[0], "exit")) { /* exit command */
                 if(pipetype == PIPEIN){ // 마지막 파이프일 경우, exit의 결과로 인터럽트 시그널을 부모에게 전달
-                    pid = getpid(); 
                     kill(pid, SIGINT);     /* 인터럽트 시그널을 발생시킨다. */
                 }
                 exit(0);
@@ -149,14 +221,16 @@ void createPipeProcess(char *cmdline, int* fd, int pipetype)
                 printf("%s: Command not found.\n", argv[0]);
                 exit(0);
             }
-        } 
+        } else{
+            return pid;
+        }
     }
-    return;
+    return -1;
 }
 
 /* $begin createProcess */
 /* createProcess - Evaluate a command line and fork chile process */
-void createProcess(char *cmdline) 
+pid_t createProcess(char *cmdline) 
 {
     char *argv[MAXARGS]; /* Argument list execve() */
     char buf[MAXLINE];   /* Holds modified command line */
@@ -169,11 +243,11 @@ void createProcess(char *cmdline)
     bg = myshell_parseline(buf, argv); 
     /* Ignore empty lines */
     if (argv[0] == NULL) {
-	    return;   
+	    return -1;   
     }
 
     if (!builtin_command(argv)) { //quit -> exit(0), & -> ignore, other -> run
-        if(Fork() == 0){
+        if((pid = Fork()) == 0){
             //child process
             if (!strcmp(argv[0], "exit")) { /* exit command */
                 pid = getpid();   
@@ -185,20 +259,11 @@ void createProcess(char *cmdline)
                 printf("%s: Command not found.\n", argv[0]);
                 exit(0);
             }
-        } else {
-            //parent
-            Wait(&status);
-            if(WIFSIGNALED(status)) {
-                /* 
-                자식 프로세스에서 시그널이 발생한 경우, 오류를 처리해 준다. 
-                phase1에서 시그널이 생기는 경우는 exit 처리 경우뿐이므로
-                exit(0)를 해주도록 단순 처리하였다.
-                */
-                exit(0);
-            }
+        }else {
+            return pid;
         }
     }
-    return;
+    return -1;
 }
 
 /* If first arg is a builtin command, run it and return true */
